af318e8d53dfe0def073d9a2a580b4edd019950c
yashodgayashan <yashodgayashan@gmail.com>
2020-03-03 17:43:08 +0530

    Added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Header />
       <Footer />
     </div>
   );
