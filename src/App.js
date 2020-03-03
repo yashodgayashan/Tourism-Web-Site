0f9b1e390a7e0635487a4dfeb3e8e31c7d98b506
yashodgayashan <yashodgayashan@gmail.com>
2020-03-03 16:56:35 +0530

    Added footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "./components/footer/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
+      <Footer />
     </div>
   );
 }
