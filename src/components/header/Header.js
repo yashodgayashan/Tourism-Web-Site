f8e8b078fa7f36fad2b383f65137c7124c09dd17
yashodgayashan <yashodgayashan@gmail.com>
2020-04-28 22:54:19 +0530

    Added links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -12,22 +12,22 @@ const Header = () => {
       <label className="logo all">DesignX</label>
       <ul className="all">
         <li className="all">
-          <a className="active" href="#" className="all">
+          <a className="active" href="/home" className="all">
             Home
           </a>
         </li>
         <li className="all">
-          <a href="#" className="all">
+          <a href="/places" className="all">
             Places to visit
           </a>
         </li>
         <li className="all">
-          <a href="#" className="all">
+          <a href="/vehicles" className="all">
             Vehicles
           </a>
         </li>
         <li className="all">
-          <a href="#" className="all">
+          <a href="/contact-us" className="all">
             Contact
           </a>
         </li>
